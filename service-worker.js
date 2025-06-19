4f5f52d48ca82564a4fdf25f4bb17254c8cc3ee3
giagiu84 <giagiu84@gmail.com>
2025-06-19 21:55:43 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,17 @@
-
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("gg84-cache").then(cache => {
-      return cache.addAll(["index.html", "app.html", "manifest.json", "logo.png"]);
-    })
+    caches.open("gg84-store").then(cache => cache.addAll([
+      "/",
+      "/index.html",
+      "/app.html",
+      "/pro.html",
+      "/messaggistica_pro.html",
+      "/manifest.json",
+      "/logo.png"
+    ]))
   );
 });
+
 self.addEventListener("fetch", e => {
   e.respondWith(
     caches.match(e.request).then(response => response || fetch(e.request))
